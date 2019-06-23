9273f5c7091c130e8d3f847ef4e13725f029bdc6
roman-geraskin <51863158+roman-geraskin@users.noreply.github.com>
2019-06-23 10:26:52 +0300

    pep8 compliance

--- monitor.py
+++ monitor.py
@@ -18,11 +18,11 @@ estimated_delay_min = 0.06
 delay_raise_by = 0.01
 delay_decrease_by = 0.01
 
-#start_delay = 3
-#estimated_delay_max = 4
-#estimated_delay_min = 2
-#delay_raise_by = 0.1
-#delay_decrease_by = 0.2
+# start_delay = 3
+# estimated_delay_max = 4
+# estimated_delay_min = 2
+# delay_raise_by = 0.1
+# delay_decrease_by = 0.2
 
 samples = 100
 samples_inc = 0
@@ -30,19 +30,21 @@ samples_inc = 0
 results = Queue()
 delays = {"Get": [], "Send": []}
 
+
 def adjust_delay(who, delay, t1, t2):
     real_delay = t2 - t1
-    #print(f"{who} set delay:", round(delay, 3))
-    #print(f"{who} real delay:", round(real_delay, 3))
-    #print()
+    # print(f"{who} set delay:", round(delay, 3))
+    # print(f"{who} real delay:", round(real_delay, 3))
+    # print()
 
     if real_delay > estimated_delay_max and delay - delay_decrease_by > 0:
         delay -= delay_decrease_by
     elif real_delay < estimated_delay_min:
         delay += delay_raise_by
-        
+
     delays[who].append(real_delay)
     return delay
+
 
 def get_measures(result):
     # I read /proc directly because atop is too slow IMO
@@ -52,7 +54,7 @@ def get_measures(result):
     while i < samples:
         i += samples_inc
         t1 = time.time()
-        
+
         # CPU
         with open("/proc/loadavg") as f:
             out = f.read()
@@ -64,78 +66,108 @@ def get_measures(result):
         # Memory
         with open("/proc/meminfo") as f:
             out = f.readlines()
-        for l in out:
-            l = l.split()
-            if l:
-                if l[0] == "MemTotal:":
-                    memtotal = int(l[1])
-                elif l[0] == "MemFree:":
-                    memfree = int(l[1])
+        for line in out:
+            line = line.split()
+            if line:
+                if line[0] == "MemTotal:":
+                    memtotal = int(line[1])
+                elif line[0] == "MemFree:":
+                    memfree = int(line[1])
         result["mem"]["free_perc"] = round(memfree / memtotal * 100, 2)
 
         # IO
         # Because of the low measures interval there will be peaks and falls.
-        # So you should look at average value or implement a measures interval increasing hack.
+        # So you should look at average value
+        # or implement a measures interval increasing hack.
         with open("/proc/diskstats") as f:
             val = f.readlines()
-        for l in val:
-            l = l.strip().split()
-            if l:
-                name = l[2]
+        for line in val:
+            line = line.strip().split()
+            if line:
+                name = line[2]
                 if result["dsk"]["_disk_to_mon"] != name:
                     continue
-                val = int(l[12]) # time spent doing I/Os (ms)
+                val = int(line[12])  # time spent doing I/Os (ms)
                 if name not in result["dsk"]:
                     result["dsk"][name] = {}
                 if "_val" in result["dsk"][name]:
-                    io_time_spent_delta = val - result["dsk"][name]["_val"]             # ms
-                    measures_time_spent_delta = (t1 - result["dsk"]["_time"])           # seconds
-                    utilization = io_time_spent_delta / measures_time_spent_delta / 10  # percent
+                    # io_time_spent_delta in ms
+                    io_time_spent_delta = val - result["dsk"][name]["_val"]
+                    # measures_time_spent_delta in seconds
+                    measures_time_spent_delta = (t1 - result["dsk"]["_time"])
+                    # utilization in percents
+                    utilization = \
+                        io_time_spent_delta / measures_time_spent_delta / 10
                     result["dsk"][name]["busy_perc"] = round(utilization, 2)
                 result["dsk"][name]["_val"] = val
         result["dsk"]["_time"] = t1
 
         # Network
         # Because of the low measures interval there may be peaks and falls.
-        # So you should look at average value or implement a measures interval increasing hack.
+        # So you should look at average value
+        # or implement a measures interval increasing hack.
         with open("/proc/net/dev") as f:
             out = f.readlines()
-        for l in out:
-            l = l.split()
-            name = l[0][:-1]
+        for line in out:
+            line = line.split()
+            name = line[0][:-1]
             if name in result["net"]:
                 if "_rx" in result["net"][name]:
-                    rx_delta = int(l[1]) - result["net"][name]["_rx"]
-                    tx_delta = int(l[9]) - result["net"][name]["_tx"]
+                    rx_delta = int(line[1]) - result["net"][name]["_rx"]
+                    tx_delta = int(line[9]) - result["net"][name]["_tx"]
                     time_delta = t1 - result["net"]["_time"]
 
                     if result["net"][name]["_full_duplex"]:
-                        result["net"][name]["_activity_perc"] = (rx_delta + tx_delta) 
+                        result["net"][name]["_activity_perc"] = \
+                            (rx_delta + tx_delta)
                     else:
-                        result["net"][name]["_activity_perc"] = max(rx_delta, tx_delta)
-                    result["net"][name]["_activity_perc"] = result["net"][name]["_activity_perc"] * 8 / 1024 / 1024 / time_delta
-                    result["net"][name]["_activity_perc"] = round(result["net"][name]["_activity_perc"] / result["net"][name]["_speed_mbps"] * 100, 2)
-                result["net"][name]["_rx"] = int(l[1])
-                result["net"][name]["_tx"] = int(l[9])
+                        result["net"][name]["_activity_perc"] = max(rx_delta,
+                                                                    tx_delta)
+                    result["net"][name]["_activity_perc"] *= \
+                        8 / 1024 / 1024 / time_delta
+                    result["net"][name]["_activity_perc"] /= \
+                        result["net"][name]["_speed_mbps"] * 100
+                    result["net"][name]["_activity_perc"] = \
+                        round(result["net"][name]["_activity_perc"], 2)
+                result["net"][name]["_rx"] = int(line[1])
+                result["net"][name]["_tx"] = int(line[9])
                 if "_activity_perc_log" in result["net"][name]:
-                    result["net"][name]["_activity_perc_log"].append(result["net"][name]["_activity_perc"])
-                    result["net"][name]["avg_perc"] = round(sum(result["net"][name]["_activity_perc_log"])/len(result["net"][name]["_activity_perc_log"]), 2)
-                    result["net"][name]["min_perc"] = round(min(result["net"][name]["_activity_perc_log"]), 2)
-                    result["net"][name]["max_perc"] = round(max(result["net"][name]["_activity_perc_log"]), 2)
-                    result["net"][name]["perc95"] = round(numpy.percentile(result["net"][name]["_activity_perc_log"], 95), 2)
-                    result["net"][name]["perc99"] = round(numpy.percentile(result["net"][name]["_activity_perc_log"], 99), 2)
-                    # BTW do not need all of this because of zabbix has it's own functional for this ^^ (min, max, avg, may be percentile too)
+                    result["net"][name]["_activity_perc_log"].append(
+                        result["net"][name]["_activity_perc"])
+                    sum_act = sum(result["net"][name]["_activity_perc_log"])
+                    len_act = len(result["net"][name]["_activity_perc_log"])
+                    result["net"][name]["avg_perc"] = \
+                        round(sum_act / len_act, 2)
+                    result["net"][name]["min_perc"] = \
+                        round(min(result["net"][name]["_activity_perc_log"]),
+                              2)
+                    result["net"][name]["max_perc"] = \
+                        round(max(result["net"][name]["_activity_perc_log"]),
+                              2)
+                    result["net"][name]["perc95"] = \
+                        round(numpy.percentile(
+                            result["net"][name]["_activity_perc_log"], 95), 2)
+                    result["net"][name]["perc99"] = \
+                        round(numpy.percentile(
+                            result["net"][name]["_activity_perc_log"], 99), 2)
+                    # BTW do not need all of this ^^ because of zabbix has
+                    # it's own functional for this
+                    # (min, max, avg, may be percentile too)
                 else:
-                    result["net"][name]["_activity_perc_log"] = []        
+                    result["net"][name]["_activity_perc_log"] = []
         result["net"]["_time"] = t1
-        
+
         # PTP
-        out = subprocess.run("sudo journalctl -b --no-pager -u ptp4l -n 50", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode().strip()
+        out = subprocess.run("sudo journalctl -b --no-pager -u ptp4l -n 50",
+                             shell=True, check=True,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE).stdout.decode().strip()
         out = out.split("\n")[1:]
         result["ptp"]["status"] = out[-1].strip()
         for l in out[::-1]:
             if "master offset" in l:
-                result["ptp"]["master_offset"] = l.split("master offset")[1].split()[0]
+                result["ptp"]["master_offset"] = \
+                    l.split("master offset")[1].split()[0]
                 break
             elif "grand master" in l:
                 result["ptp"]["master_offset"] = 0
@@ -149,6 +181,7 @@ def get_measures(result):
         results.put(result)
         delay = adjust_delay("Get", delay, t1, t2)
 
+
 def flatten(dictionary, prefix=[], result={}):
     for k, v in dictionary.items():
         if k[0] == '_':
@@ -157,20 +190,21 @@ def flatten(dictionary, prefix=[], result={}):
             flatten(v, prefix+[k], result)
         else:
             prefix_str = '_'.join(prefix + [k])
-            if not prefix_str in result:
+            if prefix_str not in result:
                 result[prefix_str] = {}
             result[prefix_str] = v
     return result
 
+
 def send_measures():
     hostname = socket.gethostname()
-    
+
     delay = start_delay
     i = 0
     while i < samples:
         i += samples_inc
         t1 = time.time()
-           
+
         result = results.get()
         fresult = flatten(result)
         for k, v in fresult.items():
@@ -178,18 +212,25 @@ def send_measures():
                 k = k.split("_")
                 del(k[1])
                 k = "_".join(k)
-            #print(k, v)
-            #subprocess.Popen(f"/usr/bin/zabbix_sender -z {zabbix_server} -s {hostname} -k {k} -o".split() + [str(v)], stdout=subprocess.DEVNULL)
-            subprocess.Popen(f"/usr/bin/zabbix_sender -z {zabbix_server} -s {hostname} -k {k} -o '{v}' >/dev/null", shell=True)
-        #print()
-        #pprint(result)
-        
+            # print(k, v)
+            # subprocess.Popen(f"/usr/bin/zabbix_sender -z {zabbix_server} "
+            #                   "-s {hostname} -k {k} -o".split() + [str(v)],
+            #                   stdout=subprocess.DEVNULL)
+            subprocess.Popen(f"/usr/bin/zabbix_sender -z {zabbix_server} "
+                             f"-s {hostname} -k {k} -o '{v}' "
+                             f">/dev/null", shell=True)
+        # print()
+        # pprint(result)
+
         # Intervals
         time.sleep(delay)
         t2 = time.time()
-        subprocess.Popen(f"/usr/bin/zabbix_sender -z {zabbix_server} -s {hostname} -k interval_send -o {t2 - t1} >/dev/null", shell=True)
+        subprocess.Popen(f"/usr/bin/zabbix_sender -z {zabbix_server} "
+                         f"-s {hostname} -k interval_send -o {t2 - t1} "
+                         f">/dev/null", shell=True)
 
         delay = adjust_delay("Send", delay, t1, t2)
+
 
 def main():
     result = {}
@@ -199,35 +240,42 @@ def main():
     result["net"] = {}
     result["ptp"] = {}
     result["interval"] = {}
-    
+
     # find out the number of cpus
     with open("/proc/cpuinfo") as f:
         out = f.read()
     result["cpl"]["_numcpu"] = out.count("processor")
 
     # find out what ifaces are full duplex, ifaces speed
-    out = subprocess.run("dmesg", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode()
+    out = subprocess.run("dmesg", shell=True, check=True,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE).stdout.decode()
     for l in out.split("\n"):
         if "Duplex," in l:
             name = l.split("]")[1].strip().split()[1]
             result["net"][name] = {}
             result["net"][name]["_full_duplex"] = "Full Duplex" in l
-            result["net"][name]["_speed_mbps"] = int(l.split("Mbps")[0].strip().split()[-1]) # are there any nics with speed counted less than Mbps?
-    
-	# find out what iface is responsible for disered net
+            # are there any nics with speed counted less than Mbps?
+            result["net"][name]["_speed_mbps"] = \
+                int(l.split("Mbps")[0].strip().split()[-1])
+
+    # find out what iface is responsible for disered net
     net_to_mon = ".".join(zabbix_server.split(".")[:3]+["255"])
     for iface in result["net"]:
-        if net_to_mon in [x['broadcast'] for x in netifaces.ifaddresses(iface)[netifaces.AF_INET]]:
+        if (net_to_mon in [x['broadcast']
+                           for x in
+                           netifaces.ifaddresses(iface)[netifaces.AF_INET]]):
             break
-    result["net"], result["net"][iface] = {}, result["net"][iface] # python rulez
-    
+    # python rulez
+    result["net"], result["net"][iface] = {}, result["net"][iface]
+
     # find out rootfs device
     with open("/proc/mounts") as f:
         conts = f.readlines()
-    for l in conts:
-        l = l.split(" ")
-        if l[1] == '/':
-            disk_to_mon = l[0].split("/")[-1]
+    for line in conts:
+        line = line.split(" ")
+        if line[1] == '/':
+            disk_to_mon = line[0].split("/")[-1]
             result["dsk"]["_disk_to_mon"] = disk_to_mon
             break
 
@@ -238,9 +286,12 @@ def main():
     send_thread.start()
     get_thread.join()
     send_thread.join()
-    
-    print("Get average time spent: ", sum(delays["Get"]) / len(delays["Get"]))
-    print("Send average time spent:", sum(delays["Send"]) / len(delays["Send"]))
+
+    print("Get average time spent: ",
+          sum(delays["Get"]) / len(delays["Get"]))
+    print("Send average time spent:",
+          sum(delays["Send"]) / len(delays["Send"]))
+
 
 if __name__ == "__main__":
     main()
--- stress.py
+++ stress.py
@@ -7,7 +7,8 @@ from random import randint
 iperf_server = "192.168.55.2"
 iperf_min_max_MB = (10, 75)
 dd_min_max_MB = (1, 10)
-dd_min_max_MB = (50, 100) # looks more 'stressy' than previous line
+dd_min_max_MB = (50, 100)  # looks more 'stressy' than previous line
+
 
 def main():
     hostname = socket.gethostname()
@@ -18,17 +19,28 @@ def main():
             out = f.read()
         ncpu = out.count("processor")
         subprocess.Popen("iperf -u -s >/dev/null", shell=True)
-        subprocess.Popen(f"stress-ng --cpu {ncpu} --vm 1 2>/dev/null", shell=True)
-        print(f"Stress started: iperf as server (udp), stress-ng --cpu {ncpu} --vm 1")
+        subprocess.Popen(f"stress-ng --cpu {ncpu} --vm 1 2>/dev/null",
+                         shell=True)
+        print(f"Stress started: iperf as server (udp),"
+              f" stress-ng --cpu {ncpu} --vm 1")
         a = input()
     elif hostname == "vm3":
         # iperf, dd
-        bandwidth = randint(iperf_min_max_MB[0] * 8, iperf_min_max_MB[1] * 8) # MB => Mb
-        subprocess.Popen(f"while : ; do iperf -u -b {bandwidth}M -c {iperf_server} >/dev/null; done", shell=True)
+        bandwidth = randint(iperf_min_max_MB[0] * 8,
+                            iperf_min_max_MB[1] * 8)  # MB => Mb
+        subprocess.Popen(f"while : ; do "
+                         f"iperf -u -b {bandwidth}M -c {iperf_server} "
+                         f">/dev/null; done", shell=True)
         throttle = randint(dd_min_max_MB[0], dd_min_max_MB[1])
-        subprocess.Popen(f"while : ; do dd if=/dev/zero bs=50M count=10 2>/dev/null | throttle -M {throttle} | dd of=/home/vagrant/dd_out 2>/dev/null; done", shell=True)
-        print(f"Stress started: iperf at {round(bandwidth/8)} MB/s (udp), dd at {throttle} MB/s")
+        subprocess.Popen(f"while : ; do "
+                         f"dd if=/dev/zero bs=50M count=10 2>/dev/null | "
+                         f"throttle -M {throttle} | "
+                         f"dd of=/home/vagrant/dd_out 2>/dev/null; done",
+                         shell=True)
+        print(f"Stress started: iperf at {round(bandwidth/8)} MB/s (udp), "
+              f"dd at {throttle} MB/s")
         a = input()
+
 
 if __name__ == "__main__":
     main()
